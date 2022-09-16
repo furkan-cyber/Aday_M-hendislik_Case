c98645bd58e404563fb73742ac13789679ff8855
Furkan Avcıoğlu <80673441+furkan-cyber@users.noreply.github.com>
2022-09-16 15:21:50 +0300

    Add files via upload

---
+++ create_table.sql
@@ -0,0 +1,37 @@
+CREATE TABLE flight (
+    flight_id INTEGER PRIMARY KEY,
+	model_id  INTEGER,
+	mission_id INTEGER,
+	flight_date TIMESTAMP,
+	landing_date TIMESTAMP,
+	landing_success VARCHAR(1),
+	flight_success  VARCHAR(1),
+	flight_county VARCHAR(15),
+	flight_province VARCHAR(15),
+	flight_region VARCHAR(25),
+	real_flight_time TIME ,
+	mission_success  VARCHAR(1)
+);
+
+CREATE TABLE gps(
+    flight_id INTEGER,
+	timee TIME,
+	lat   FLOAT,
+	lon   FLOAT,
+	alt   FLOAT
+);
+
+CREATE TABLE ang_vel(
+     flight_id INTEGER,
+     timee     TIMESTAMP,
+	 xyz0      FLOAT,
+	 xyz1      FLOAT,
+	 xyz2      FLOAT,
+	 typee     VARCHAR(50)
+);
+
+
+
+
+
+
