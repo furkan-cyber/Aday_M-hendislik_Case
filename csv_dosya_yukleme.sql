04a41ee3840d70847c1d21ed1d6fc7b840eb55e3
Furkan Avcıoğlu <80673441+furkan-cyber@users.noreply.github.com>
2022-09-16 16:17:35 +0300

    Add files via upload

---
+++ csv_dosya_yukleme.sql
@@ -0,0 +1,22 @@
+COPY flight
+FROM 'C:\Users\PC\Desktop\Task (1)\flight.csv' 
+DELIMITER ',' 
+CSV HEADER;
+
+SELECT * FROM flight
+
+
+
+COPY gps
+FROM 'C:\Users\PC\Desktop\Task (1)\gps.csv' 
+DELIMITER ',' 
+CSV HEADER;
+
+SELECT * FROM gps
+
+COPY ang_vel
+FROM 'C:\Users\PC\Desktop\Task (1)\angular_velocity.csv' 
+DELIMITER ',' 
+CSV HEADER;
+
+SELECT * FROM ang_vel
